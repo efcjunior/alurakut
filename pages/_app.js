3ca582135a52f368d3f93ce90037e23fc526f9f9
Everson Ferreira da Cunha Junior <c114416@corp.caixa.gov.br>
2021-07-12 20:36:28 -0300

    alurakut dia 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  /* Reset CSS (Necolas Reset CSS <3) */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
+`
+
+const theme = {
+  colors: {
+    primary: 'red',
+  },
 }
 
-export default MyApp
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
